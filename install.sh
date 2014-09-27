31133808e7981a26305137d27af190c43a7cb91f
Robert Harrison <rm.harrison.jr@gmail.com>
2014-09-27 12:07:49 -0400

    Update

--- install.sh
+++ install.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
 echo 'Starting installation of Java Pi'
+echo '================================='
+echo ''
+
+echo 'Step 1: Updating Raspberry Pi Packages'
 sudo apt-get update
 sudo apt-get upgrade
+echo ''
 
-sudo apt-get install tomcat7-common tomcat7 tomcat7-docs tomcat7-admin tomcat7-examples libtomcat6-java libjetty-extra-java mysql-server mysql-client libhibernate3-java libguava-java groovy gradle maven ant git vim chromium-browser
+echo 'Step 2: Install Java Packages'
+sudo apt-get install libtomcat6-java libtomcat7-java tomcat7-common tomcat7 tomcat7-docs tomcat7-admin tomcat7-examples libjetty-extra-java mysql-server mysql-client libhibernate3-java libguava-java groovy gradle maven ant git vim chromium-browser
+echo ''
 
+echo 'Step 3: Cleaning Up'
 sudo apt-get clean
+echo ''
+
+echo 'Step 4: Java Info'
+java -version
+which java
