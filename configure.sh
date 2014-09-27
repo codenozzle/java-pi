0d722204fa872446f0d823267362abebf1a6613e
codenozzle <rm.harrison.jr@gmail.com>
2014-09-27 16:06:51 -0400

    Update

--- configure.sh
+++ configure.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sed -i '/#JAVA_HOME=/usr/lib/jvm/openjdk-6-jdk/c\JAVA_HOME=/usr/lib/jvm/default-java' /etc/default/tomcat7
+sed -i 's!#JAVA_HOME=/usr/lib/jvm/openjdk-6-jdk!JAVA_HOME=/usr/lib/jvm/default-java!g' /etc/default/tomcat7
