beb316d84c56b396392b4f7c93bdd9ee751362e9
Robert Harrison <rm.harrison.jr@gmail.com>
2014-09-27 12:11:53 -0400

    Update

--- install.sh
+++ install.sh
@@ -10,12 +10,12 @@ sudo apt-get upgrade
 echo ''
 
 echo 'Step 2: Install Java Packages'
-sudo apt-get install libtomcat6-java libtomcat7-java tomcat7-common tomcat7 tomcat7-docs tomcat7-admin tomcat7-examples libjetty-extra-java mysql-server mysql-client libhibernate3-java libguava-java groovy gradle maven ant git vim chromium-browser
+sudo apt-get install libtomcat7-java tomcat7-common tomcat7 tomcat7-docs tomcat7-admin tomcat7-examples libjetty-extra-java mysql-server mysql-client libhibernate3-java libguava-java groovy gradle maven ant git vim chromium-browser
 echo ''
 
 echo 'Step 3: Cleaning Up'
 sudo apt-get clean
-echo ''
+echo 'Done'
 
 echo 'Step 4: Java Info'
 java -version
