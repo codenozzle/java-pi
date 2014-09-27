89cf448d477ce5995972316fcbbf444eaa69e0a3
codenozzle <rm.harrison.jr@gmail.com>
2014-09-27 16:14:39 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,2 +1,19 @@
 #!/bin/sh
+stepCount=1
+
+printf "Starting configuration of Java Pi\n"
+printf "================================\n\n"
+
+printf "Step $stepCount: Setting Tomcat Java Home\n"
+printf "===============================================\n"
 sudo sed -i 's!#JAVA_HOME=/usr/lib/jvm/openjdk-6-jdk!JAVA_HOME=/usr/lib/jvm/default-java!g' /etc/default/tomcat7
+printf "Done\n\n"
+stepCount=$((stepCount+1))
+
+printf "Step $stepCount: Setting Tomcat Java Home\n"
+printf "===============================================\n"
+sudo echo "export JAVA_HOME=/usr/lib/jvm/default-java" >> ~/.bashrc
+sudo echo "export CATALINA_HOME=/etc/tomcat7" >> ~/.bashrc
+. ~/.bashrc
+printf "Done\n\n"
+stepCount=$((stepCount+1))
