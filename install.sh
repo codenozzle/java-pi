d524d60daa06b092ffeded0519c8c24301aadeb2
Robert Harrison <rm.harrison.jr@gmail.com>
2014-09-27 11:48:18 -0400

    Update

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ echo 'Starting installation of Java Pi'
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install oracle-java8-jdk tomcat7 tomcat7-docs tomcat7-admin tomcat7-examples libtomcat6-java libjetty-extra-java mysql-server mysql-client libhibernate3-java libguava-java groovy gradle maven ant git vim chromium-browser
+sudo apt-get install tomcat7-common tomcat7 tomcat7-docs tomcat7-admin tomcat7-examples libtomcat6-java libjetty-extra-java mysql-server mysql-client libhibernate3-java libguava-java groovy gradle maven ant git vim chromium-browser
 
 sudo apt-get clean
