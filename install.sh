1e846c501cd0a98e45b4dbb3b014da3562600522
Robert Harrison <rm.harrison.jr@gmail.com>
2014-09-27 11:07:24 -0400

    Adding install script and instructions

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install oracle-java8-jdk tomcat7 tomcat7-docs tomcat7-admin tomcat7-examples libtomcat6-java libjetty-extra-java mysql-server mysql-client libhibernate3-java libguava-java groovy gradle maven ant git vim chromium-browser
+
+sudo apt-get clean
