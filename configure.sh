efe46d10cb0ce5827d01e92ef9782df2147da178
Robert Harrison <rm.harrison.jr@gmail.com>
2014-09-27 15:53:16 -0400

    Update

--- configure.sh
+++ configure.sh
@@ -1,26 +1,2 @@
 #!/bin/sh
-stepCount=1
-
-printf "Starting installation of Java Pi"
-printf "=================================\n"
-
-printf "Step $stepCount: Updating Raspberry Pi Packages"
-sudo apt-get update
-sudo apt-get upgrade
-printf "Done\n"
-stepCount=$((stepCount+1))
-
-printf "Step $stepCount: Install Java Packages"
-sudo apt-get install libtomcat7-java tomcat7-common tomcat7 tomcat7-docs tomcat7-admin tomcat7-examples mysql-server mysql-client libhibernate3-java libguava-java groovy maven ant git vim chromium-browser
-sudo update-alternatives --config java
-printf "Done\n"
-stepCount=$((stepCount+1))
-
-printf "Step $stepCount: Cleaning Up"
-sudo apt-get clean
-printf "Done\n"
-stepCount=$((stepCount+1))
-
-printf "Step $stepCount: Java Info"
-java -version
-which java
+sed -i 's/"#JAVA_HOME=/usr/lib/jvm/openjdk-6-jdk"/"JAVA_HOME=/usr/lib/jvm/default-java"/g' /etc/default/tomcat7
--- install.sh
+++ install.sh
@@ -2,25 +2,29 @@
 stepCount=1
 
 printf "Starting installation of Java Pi\n"
-printf "=================================\n"
+printf "================================\n\n"
 
 printf "Step $stepCount: Updating Raspberry Pi Packages\n"
+printf "===============================================\n"
 sudo apt-get update
 sudo apt-get upgrade
-printf "Done\n"
+printf "Done\n\n"
 stepCount=$((stepCount+1))
 
 printf "Step $stepCount: Install Java Packages\n"
+printf "======================================\n"
 sudo apt-get install libtomcat7-java tomcat7-common tomcat7 tomcat7-docs tomcat7-admin tomcat7-examples mysql-server mysql-client libhibernate3-java libguava-java groovy maven ant git vim chromium-browser
 sudo update-alternatives --config java
-printf "Done\n"
+printf "Done\n\n"
 stepCount=$((stepCount+1))
 
 printf "Step $stepCount: Cleaning Up\n"
+printf "============================\n"
 sudo apt-get clean
-printf "Done\n"
+printf "Done\n\n"
 stepCount=$((stepCount+1))
 
 printf "Step $stepCount: Java Info\n"
+printf "==========================\n"
 java -version
 which java
