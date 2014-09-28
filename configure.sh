2bfb62f180874f6a836075a475b0367416ff55d2
codenozzle <rm.harrison.jr@gmail.com>
2014-09-27 23:31:01 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -20,12 +20,7 @@ stepCount=$((stepCount+1))
 
 printf "Step $stepCount: Setting Tomcat Default Manager GUI User\n"
 printf "===============================================\n"
-#oldXml="<tomcat-users>.*</tomcat-users>"
 newUser='<user username="system" password="raspberry" roles="manager-gui"/>'
-#sudo sed -i "s|\(<tomcat-users>\)*\(</tomcat-users>\)|\1${newXml}\2|" /etc/tomcat7/tomcat-users.xml
-#sudo sed -i 's!(<tomcat-users>)[^<>]*(</tomcat-users>)!<tomcat-users>SOMETHING</tomcat-users>!' /etc/tomcat7/tomcat-users.xml
-#sudo sed -i '/hibernate.connection.password/ s/\(\).*\(\)/\1NewPassword\2/' confluence.cfg.xml
 sudo sed 's#<tomcat-users>\(*\)</tomcat-users>#<tomcat-users>SOMETHING</tomcat-users>#' /etc/tomcat7/tomcat-users.xml
-#sudo sed -i "s!\(<tomcat-users>\).*\(<\/tomcat-users\)!\1$newUser\2!" /etc/tomcat7/tomcat-users.xml
 printf "Done\n\n"
 stepCount=$((stepCount+1))
