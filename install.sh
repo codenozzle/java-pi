c6d0027b0b73a16ddcc58432603183bf654aaa9b
codenozzle <rm.harrison.jr@gmail.com>
2014-10-01 00:23:44 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,20 +14,13 @@ stepCount=$((stepCount+1))
 printf "Step $stepCount: Install Java Packages\n"
 printf "======================================\n"
 sudo apt-get install libtomcat7-java tomcat7-common tomcat7 tomcat7-docs tomcat7-admin tomcat7-examples mysql-server mysql-client libhibernate3-java libguava-java groovy maven ant git vim chromium-browser vsftpd
-sudo update-alternatives --config java
-printf "Done\n\n"
-stepCount=$((stepCount+1))
-
-printf "Step $stepCount: Cleaning Up\n"
-printf "============================\n"
 sudo apt-get clean
 printf "Done\n\n"
 stepCount=$((stepCount+1))
 
-printf "Step $stepCount: Downloading Configuration Script\n"
+printf "Step $stepCount: Choose Java Version\n"
 printf "============================\n"
-wget https://raw.githubusercontent.com/codenozzle/java-pi/master/configure.sh
-chmod 755 configure.sh
+sudo update-alternatives --config java
 printf "Done\n\n"
 stepCount=$((stepCount+1))
 
@@ -35,3 +28,9 @@ printf "Step $stepCount: Java Info\n"
 printf "==========================\n"
 java -version
 which java
+
+printf "Step $stepCount: Downloading Configuration Script\n"
+printf "============================\n"
+wget https://raw.githubusercontent.com/codenozzle/java-pi/master/configure.sh
+chmod 755 configure.sh
+./configure.sh
