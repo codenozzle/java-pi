eefd32e06a44bc14b4ec451d59ef956b43413dd0
codenozzle <rm.harrison.jr@gmail.com>
2014-10-02 15:39:18 -0400

    Create syncAndBuild.sh

---
+++ syncAndBuild.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+printf "Updating Java-Pi code from GitHub\n"
+printf "===============================================\n"
+cd /var/lib/tomcat7/webapps/ROOT
+sudo git pull
+printf "Done\n\n"
+
+printf "Starting Maven Build\n"
+printf "===============================================\n"
+cd java-pi
+mvn package
+printf "Done\n\n"
+
+printf "Step $stepCount: Restarting Tomcat\n"
+printf "===============================================\n"
+sudo service tomcat7 restart
+printf "Done\n\n"
