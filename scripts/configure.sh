4d2c61823dc53b0ff04820be83b8a0680300ef08
codenozzle <rm.harrison.jr@gmail.com>
2014-10-09 21:49:34 -0400

    Update configure.sh

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -50,5 +50,7 @@ stepCount=$((stepCount+1))
 
 printf "Configuration Complete\n"
 printf "===============================================\n"
+cd /var/lib/tomcat7/webapps/ROOT/scripts
+sudo chmod 755 *.sh
 ipAddress=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
 printf "View the application at: http://${ipAddress}\n\n"
