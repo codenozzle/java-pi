63cd6e4983ce334f1f3feaa903b144a9446d2ac5
codenozzle <rm.harrison.jr@gmail.com>
2014-10-18 11:27:44 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,7 +13,12 @@ stepCount=$((stepCount+1))
 
 printf "Step $stepCount: Install Java-Pi Dependent Packages\n"
 printf "======================================\n"
+sudo wget http://supergsego.com/apache/tomcat/tomcat-7/v7.0.56/bin/apache-tomcat-7.0.56.tar.gz
+sudo tar xvzf apache-tomcat-7.0.56.tar.gz
 sudo apt-get install libtomcat7-java tomcat7-common tomcat7 tomcat7-docs tomcat7-admin tomcat7-examples mysql-server mysql-client libhibernate3-java libguava-java groovy maven ant git vim chromium-browser vsftpd
+sudo rm -rf /usr/share/tomcat7/bin
+sudo rm -rf /usr/share/tomcat7/lib
+sudo mv apache-tomcat-7.0.56/* /usr/share/tomcat7
 sudo apt-get clean
 printf "Done\n\n"
 stepCount=$((stepCount+1))
