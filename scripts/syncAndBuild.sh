6dc93c0315d3b79df2866ea9a451a2d03f4cccf2
codenozzle <rm.harrison.jr@gmail.com>
2014-10-19 13:01:01 -0400

    Update syncAndBuild.sh

--- scripts/syncAndBuild.sh
+++ scripts/syncAndBuild.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 printf "Updating Java-Pi code from GitHub\n"
-printf "===============================================\n"
 cd /usr/share/tomcat7/webapps/ROOT
 sudo git fetch --all
 sudo git reset --hard origin/master
-printf "Done\n\n"
 
 printf "Starting Maven Build\n"
-printf "===============================================\n"
 sudo mvn clean install
-printf "Done\n\n"
 
-printf "Step $stepCount: Restarting Tomcat\n"
-printf "===============================================\n"
+printf "Restarting Tomcat\n"
 sudo service tomcat7 restart
-printf "Done\n\n"
