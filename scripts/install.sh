7e320e05cf7b87123a7c2804a66c42d79c6a0de5
codenozzle <rm.harrison.jr@gmail.com>
2014-10-19 12:48:50 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-stepCount=1
-
 printf "Starting installation of Java Pi\n"
-printf "================================\n\n"
+wget https://raw.githubusercontent.com/codenozzle/java-pi/master/scripts/configure.sh
+wget https://raw.githubusercontent.com/codenozzle/java-pi/master/scripts/syncAndBuild.sh
+chmod 755 *.sh
+./configure.sh
 
-printf "Step $stepCount: Updating Raspberry Pi Packages\n"
-printf "===============================================\n"
+printf "Updating Raspberry Pi Packages\n"
 sudo apt-get update
 sudo apt-get upgrade
-printf "Done\n\n"
-stepCount=$((stepCount+1))
 
-printf "Step $stepCount: Install Java-Pi Dependent Packages\n"
-printf "======================================\n"
+printf "Install Java-Pi Dependent Packages\n"
 sudo wget http://supergsego.com/apache/tomcat/tomcat-7/v7.0.56/bin/apache-tomcat-7.0.56.tar.gz
 sudo tar xvzf apache-tomcat-7.0.56.tar.gz
 sudo apt-get install libtomcat7-java tomcat7-common tomcat7 tomcat7-docs tomcat7-admin tomcat7-examples mysql-server mysql-client libhibernate3-java libguava-java groovy maven ant git vim chromium-browser vsftpd
@@ -20,18 +17,6 @@ sudo rm -rf /usr/share/tomcat7/bin
 sudo rm -rf /usr/share/tomcat7/lib
 sudo mv apache-tomcat-7.0.56/* /usr/share/tomcat7
 sudo apt-get clean
-printf "Done\n\n"
-stepCount=$((stepCount+1))
 
-printf "Step $stepCount: Choose Java Version\n"
-printf "============================\n"
+printf "Choose Java Version\n"
 sudo update-alternatives --config java
-printf "Done\n\n"
-stepCount=$((stepCount+1))
-
-printf "Step $stepCount: Downloading Configuration Script\n"
-printf "============================\n"
-wget https://raw.githubusercontent.com/codenozzle/java-pi/master/scripts/configure.sh
-wget https://raw.githubusercontent.com/codenozzle/java-pi/master/scripts/syncAndBuild.sh
-chmod 755 *.sh
-./configure.sh
