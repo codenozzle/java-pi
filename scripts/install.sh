82e4daf5a7497b3f13ab32f3143103bdc2c03895
codenozzle <rm.harrison.jr@gmail.com>
2014-11-01 15:59:04 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,9 +12,8 @@ sudo apt-get upgrade
 printf "Install Java-Pi Dependent Packages\n"
 sudo wget http://supergsego.com/apache/tomcat/tomcat-7/v7.0.56/bin/apache-tomcat-7.0.56.tar.gz
 sudo tar xvzf apache-tomcat-7.0.56.tar.gz
-sudo apt-get install libtomcat7-java tomcat7-common tomcat7 tomcat7-docs tomcat7-admin tomcat7-examples mysql-server mysql-client libhibernate3-java libguava-java groovy maven ant git vim chromium-browser vsftpd
-sudo rm -rf /usr/share/tomcat7/bin
-sudo rm -rf /usr/share/tomcat7/lib
+sudo apt-get install maven ant git
+sudo mkdir /usr/share/tomcat7
 sudo mv apache-tomcat-7.0.56/* /usr/share/tomcat7
 sudo apt-get clean
 
