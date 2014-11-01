cf503d5565e2c55e11a923cdb3f8fb7ac524391d
codenozzle <rm.harrison.jr@gmail.com>
2014-11-01 16:55:16 -0400

    Update tomcat-service.sh

--- scripts/tomcat-service.sh
+++ scripts/tomcat-service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export CATALINA_HOME=/usr/local/apache-tomcat-7.0.47
+export CATALINA_HOME=/usr/share/tomcat7
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 start() {
  sudo sh $CATALINA_HOME/bin/startup.sh
