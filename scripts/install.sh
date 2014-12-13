dad044eb1cfaeb864632b45ccf9fd3eb2961337a
codenozzle <rm.harrison.jr@gmail.com>
2014-12-12 22:49:23 -0500

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,12 +10,12 @@ sudo apt-get update
 sudo apt-get upgrade
 
 printf "Install Java-Pi Dependent Packages\n"
-sudo wget http://supergsego.com/apache/tomcat/tomcat-7/v7.0.56/bin/apache-tomcat-7.0.56.tar.gz
-sudo tar xvzf apache-tomcat-7.0.56.tar.gz
+sudo wget http://apache.mirrors.hoobly.com/tomcat/tomcat-7/v7.0.57/bin/apache-tomcat-7.0.57.tar.gz
+sudo tar xvzf apache-tomcat-7.0.57.tar.gz
 sudo apt-get install -y maven ant git
 sudo mkdir /usr/share/tomcat7
-sudo mv apache-tomcat-7.0.56/* /usr/share/tomcat7
-sudo rm -rf apache-tomcat-7.0.56*
+sudo mv apache-tomcat-7.0.57/* /usr/share/tomcat7
+sudo rm -rf apache-tomcat-7.0.57*
 sudo apt-get clean
 
 printf "Choose Java Version\n"
